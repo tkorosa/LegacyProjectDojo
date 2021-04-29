5e6f354225e64ba416a997af3ad468c1fcbc4b25
Thomas Korosa <thomas.korosa@techtalk.at>
2021-04-29 23:17:21 +0200

    Renamed TestBootstrapper to DojoBootstrapper

--- LegacyProject/Persistence/Context/IContext.cs
+++ LegacyProject/Persistence/Context/IContext.cs
@@ -12,7 +12,6 @@ namespace Persistence.Context
     {
         DbSet<Customer> Customers { get; set; }
         DbSet<Address> AddressList { get; set; }
-        
         int SaveChanges();
     }
 }
